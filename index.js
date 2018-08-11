9e73ad38130ebc6c8ee6b6f0e89622bcc514a7a9
Eugenia <eugenia.kaloshina@gmail.com>
2018-08-11 17:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value)  {
   return object
 }
 
-
-function deleteFromObjectByKey(object,key)  {
 Object.assign({},object)
+function deleteFromObjectByKey(object,key)  {
+
 delete object.key
 return object
 }
